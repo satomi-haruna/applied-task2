aee1c2a1b3429aadd4960219cde0937de09a90e8
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-01 11:25:42 +0000

    [Update] 応用課題3コメント機能create修正

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -2,8 +2,8 @@ class BookCommentsController < ApplicationController
 
   def create
     book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_new_params)
-    # comment = Book.new(book_comment_params)  上は下の2行をまとめた1文
+    comment = current_user.book_comments.new(book_comment_params)
+    # comment = BookComment.new(book_comment_params)  上は下の2行をまとめた1文
     # comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
@@ -18,8 +18,8 @@ class BookCommentsController < ApplicationController
 
   private
 
-  def book_params
-    params.require(:book).permit(:comment)
+  def book_comment_params
+    params.require(:book_comment).permit(:comment)
   end
 
 end
