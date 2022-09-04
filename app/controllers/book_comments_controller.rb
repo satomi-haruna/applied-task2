24e66aa5303b4194ca2be7e17ec32b3831e33266
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-04 21:32:36 +0000

    [Finish?] 応用課題6 コメント機能の非同期通信化

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,22 +1,28 @@
 class BookCommentsController < ApplicationController
 
   def create
+    @book = Book.find(params[:book_id])
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     # comment = BookComment.new(book_comment_params)  上は下の2行をまとめた1文
     # comment.user_id = current_user.id
     comment.book_id = book.id
     comment.save
-    redirect_back(fallback_location: root_path)
+    # 非同期通信化のためにredict先を変更
+    # redirect_back(fallback_location: root_path)
+    # render :book_comments
     # バリデーションつける際に使用 ↓
     # else
-    #   render 'books/show'
+      # render 'books/show'
     # end
   end
 
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_back(fallback_location: root_path)
+    # 非同期通信化のためにredirect先を変更
+    # redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    # render :book_comments
   end
 
 
