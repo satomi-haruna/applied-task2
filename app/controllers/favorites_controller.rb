f8b638f51cf1dd3214bd1a55a7963d42d48c777a
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-04 13:07:05 +0000

    [Add] 応用課題6 いいね機能非同期通信化OK

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     # 前のページに戻るとき、redirect_backを使う。fallback_location:は上手く戻れなかったとき用のパスらしい
     # redirect_back(fallback_location: root_path)　非同期通信にするためコメントアウト
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     # redirect_back(fallback_location: root_path)　非同期通信にするためコメントアウト
   end
