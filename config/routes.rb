2bfa59e97a51757d3c0f6731a5d5b9ceeae498c9
satomi-haruna <satomi.haruna1124@gmail.com>
2022-09-10 06:40:55 +0000

    [Add] ゲストログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,10 @@ Rails.application.routes.draw do
   # 検索ボタンが押されると、Searchesコントローラのsearchアクションが実行される
   get 'search' => 'searches#search'
 
+  # ゲストユーザーでのログインの実装
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
